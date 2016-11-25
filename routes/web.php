a25129f8272cecef345d4546cedf1255abfd6acf
Manan Jadhav <curosmj@gmail.com>
2016-11-25 19:20:15 +0530

    add CRUD

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,48 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'users'], function () {
+    Route::get('/add', function (\Illuminate\Http\Request $request) {
+
+        DB::connection()->table('users')->insert([
+            'name' => $request->get('name'),
+            'email' => $request->get('email'),
+            'password' => $request->get('password')
+        ]);
+
+        return "inserted";
+    });
+
+    Route::get('/list', function () {
+
+        $names = DB::connection()->table('users')->select(['name'])->get();
+        print_r($names);
+
+        $all = DB::connection()->table('users')->get();
+        print_r($all);
+
+        return "list complete";
+    });
+
+
+    Route::get('/search', function () {
+
+        $search = Request::get('search');
+
+        $stmt = DB::connection()->table('users')->where('name', $search);
+        $all = $stmt->get();
+        print_r("SQL Query : \n".$stmt->toSql()."\n\n\n");
+        print_r($all);
+
+        return "search complete";
+    });
+
+
+    Route::get('/delete', function () {
+
+        DB::connection()->table('users')->delete();
+
+        return "deleted";
+    });
+});
